244708fa9175e06bd1fc06f186685e49597c3b13
GiantDrone8892 <andy080892@yahoo.de>
2016-01-23 17:29:33 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Before executing the cacheSolve function, create a square matrix with, for example:
+## > a<-makeCacheMatrix()
+## > a$set(matrix(runif(16), 4, 4)
 
+
+makeCacheMatrix <- function(x = matrix()) {
+        cache<-NULL                             ## making sure nothing is stored in Cache
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve   ## first time inverse the matrix and store to cache
+        getmatrix<-function() m                 
+        list(set=set, get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        m<-x$getmatrix()                        ## fetching the inverse matrix from cache
+        if(!is.null(m)){                        ## if the m is stored, return the inverse matrix from cache
+                return(m)
+        }
+        matrix<-x$get()                         ## otherwise inverse matrix manually
+        m<-solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
